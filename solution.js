46b505925e9b644538d230a2efc13fc08dd4f7ee
Tyrell <tyrellkamara-oliver@pursuit.org>
2024-01-05 13:47:19 -0500

    created a LinkedList class and an instance of that class

--- solution.js
+++ solution.js
@@ -12,9 +12,20 @@ const firstNode = new Node(3)
 const secondNode = new Node(6)
 const thirdNode = new Node(9)
 
-console.log(firstNode)
+// console.log(firstNode)
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head
+  }
+  
+}
+
+const list = new LinkedList()
+
+console.log(list)
 
 module.exports = {
   Node,
-  // LinkedList,
+  LinkedList,
 };
