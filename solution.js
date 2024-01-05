baca1f0e8e7825d70a4b0041c2cb095878a0c6bb
Tyller Mendoza <tyllermendoza@gmail.com>
2024-01-05 12:43:51 -0500

    half of the test passing

--- solution.js
+++ solution.js
@@ -1,5 +1,57 @@
 const { nums, words } = require("./data/data.js");
+class Node {
+  constructor(data){
+    this.data = data
+    this.next = null
+  }
+}
 
+const node1 = new Node()
+
+class LinkedList {
+  constructor(head = null){
+    this.head = head
+  }
+  insert(word) {
+    let newWord = new Node(word);
+    if (!this.head) {
+      this.head = newWord;
+    } else {
+      newWord.next = this.head;
+      this.head = newWord;
+    }
+  }
+  size() {
+    let count = 0;
+    let currentNum = this.head;
+    while (currentNum) {
+      count++;
+      currentNum = currentNum.next;
+    }
+    return count;
+  }
+  delete(data) {
+    let newWord = this.head;
+    let counter = 0;
+    while (newWord.data !== data && newWord.next) {
+      counter++;
+      newWord = newWord.next;
+    }
+    let foundWord = newWord;
+    newWord = this.head;
+    for (let i = 1; i < counter; i++) {
+      newWord = newWord.next;
+    }
+    newWord.next = foundWord.next;
+  }
+  clear() {
+    this.head = null;
+  }
+  
+}
+
+const list = new LinkedList()
+list.insert(node1)
 module.exports = {
   Node,
   LinkedList,
