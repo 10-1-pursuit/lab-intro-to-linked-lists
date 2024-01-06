588079b5a402d9bcf598bcfe445dfb830fbe1064
JackieC1993 <jackiecheung@pursuit.org>
2024-01-05 23:01:52 -0500

    added the node and insert data

--- solution.js
+++ solution.js
@@ -1,4 +1,26 @@
 const { nums, words } = require("./data/data.js");
+ 
+class Node {
+  constructor(data){
+    this.data = data;
+    this.data= null;
+ }
+}
+class LinkedList {
+  constructor(){
+    this.head= null;
+  }
+  insert(data){
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else{
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+}
 
 module.exports = {
   Node,
