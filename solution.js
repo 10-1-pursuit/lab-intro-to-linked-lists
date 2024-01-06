9386bd946890b79ce58fb011814aded4b42086a3
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-01-05 21:47:36 -0500

    Creating new nodes for all numbers

--- solution.js
+++ solution.js
@@ -5,8 +5,33 @@ class Node {
     this.data = data;
     this.next = null;
   }
-
 }
+//Creating new nodes
+const node1 = new Node(1)
+const node2 = new Node(2)
+const node3 = new Node(3)
+const node4 = new Node(4)
+const node5 = new Node(5)
+const node6 = new Node(6)
+const node7 = new Node(7)
+const node8 = new Node(8)
+const node9 = new Node(9)
+const node0 = new Node(0)
+
+//Linking the nodes the annoying way
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+node7.next = node8
+node8.next = node9
+node9.next = node0
+
+
+console.log(nums)
+console.log(words)
 
 
 
