580d70d8a1a9aa1b6e88f4cce29d93ebb2384dff
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-01-05 22:57:53 -0500

    Added Insert but code is broken

--- solution.js
+++ solution.js
@@ -1,45 +1,100 @@
 const { nums, words } = require("./data/data.js");
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+"The",
+  "quick",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+
+  class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+//Creating new nodes
+const node1 = new Node(1);
+const node2 = new Node(2);
+const nodeWord1 = new Node("The");
+const node3 = new Node(3);
+const nodeWord2 = new Node("quick");
+const node4 = new Node(4);
+const nodeWord3 = new Node("brown");
+const node5 = new Node(5);
+const nodeWord4 = new Node("fox");
+const node6 = new Node(6);
+const nodeWord5 = new Node("jumps");
+const node7 = new Node(7);
+const nodeWord6 = new Node("overs");
+const node8 = new Node(8);
+const nodeWord7 = new Node("the");
+const node9 = new Node(9);
+const nodeWord8 = new Node("lazy");
+const node0 = new Node(0);
+const nodeWord9 = new Node("dog");
+
+// //Linking the nodes the annoying way
+// node1.next = node2
+// node2.next = node3
+// node3.next = node4
+// node4.next = node5
+// node5.next = node6
+// node6.next = node7
+// node7.next = node8
+// node8.next = node9
+// node9.next = node0
+
+
+// console.log(nums)
+// console.log(words)
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head
+  }
+
+  insert(node) {
+    let currentNode = this.head;
+    if (!currentNode) {
+      currentNode = node
+    } else if (currentNode.next) {
+      while (currentNode.next) {
+        currentNode = currentNode.next
+      }
+      currentNode.next = node
+    } else {
+      currentNode.next = node
+    }
   }
 }
-//Creating new nodes
-const node1 = new Node(1)
-const node2 = new Node(2)
-const node3 = new Node(3)
-const node4 = new Node(4)
-const node5 = new Node(5)
-const node6 = new Node(6)
-const node7 = new Node(7)
-const node8 = new Node(8)
-const node9 = new Node(9)
-const node0 = new Node(0)
 
-//Linking the nodes the annoying way
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
-node5.next = node6
-node6.next = node7
-node7.next = node8
-node8.next = node9
-node9.next = node0
+//New list
 
-
-console.log(nums)
-console.log(words)
-
-
-
-
-
-
-
-
+const newList = new LinkedList() {
+  newList.insert(node1)
+newList.insert(node2)
+newList.insert(nodeWord1)
+newList.insert(node3)
+newList.insert(nodeWord2)
+newList.insert(node4)
+newList.insert(nodeWord3)
+newList.insert(node5)
+newList.insert(nodeWord4)
+newList.insert(node6)
+newList.insert(nodeWord5)
+newList.insert(nodeWord6)
+newList.insert(node7)
+newList.insert(nodeWord7)
+newList.insert(nodeWord8)
+newList.insert(node8)
+newList.insert(nodeWord9)
+newList.insert(node9)
+newList.insert(node0)
+}
 
 
 module.exports = {
