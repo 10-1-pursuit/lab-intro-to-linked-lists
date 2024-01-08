1dadabf9a1bba98a36d0eb72cd826efd8a86f127
Jesse Chong <jessechong@pursuit.org>
2024-01-08 09:32:50 -0500

    Finished getKth, isEmpty, clear

--- solution.js
+++ solution.js
@@ -41,11 +41,11 @@ class LinkedList {
     if (currentNode.data == dataProp) {
       this.head = currentNode.next;
     } else if (currentNode.next.data === dataProp) {
-      console.log("Current Node: ", currentNode);
-      console.log("currentNode.next: ", currentNode.next);
-      console.log("currentNode.next.next: ", currentNode.next.next);
+      // console.log("Current Node: ", currentNode);
+      // console.log("currentNode.next: ", currentNode.next);
+      // console.log("currentNode.next.next: ", currentNode.next.next);
       currentNode.next = currentNode.next.next;
-      console.log ("New list:", this);
+      // console.log ("New list:", this);
     }
     currentNode = currentNode.next;
   }
@@ -71,20 +71,42 @@ class LinkedList {
         currentNode = currentNode.next;
       }
     }
-    return null
+    return null;
   }
+
+  getKth(k) {
+    if(k > this.size()) return null;
+    let counter = 1;
+    let currentNode = this.head;
+    while(k !== counter) {
+      counter++;
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  isEmpty() {
+    return !this.head;
+  }
+
+  clear(){
+    this.head = null;
+  }  
 }
 
 const list = new LinkedList()
 list.insert(node1)
 list.insert(node2)
 list.insert(node3)
+list.clear()
+console.log(list.isEmpty())
 // list.delete(13)
-console.log(list.search(node1))
+// console.log(list.search(node1))
+// console.log(list.getKth(1))
 // console.log(list.size())
 // console.log(list.getFirst())
 // console.log(list.getLast())
-// console.log(list)
+console.log(list)
 
 
 
