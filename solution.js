d544259fedf606c0008dd2ce45285d37d9eb20a3
Jesse Chong <jessechong@pursuit.org>
2024-01-08 07:36:21 -0500

    Finished Node and Linkedlist

--- solution.js
+++ solution.js
@@ -1,5 +1,24 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+const node = new Node(8);
+console.log(node)
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+}
+
+const list = new LinkedList()
+console.log(list);
+
 module.exports = {
   Node,
   LinkedList,
