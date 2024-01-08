08018ea8db99c6b0a458443f7c6c785f2cf63be3
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-01-07 20:53:05 -0500

    Passed 3 tests

--- solution.js
+++ solution.js
@@ -1,59 +1,11 @@
 const { nums, words } = require("./data/data.js");
 
-"The",
-  "quick",
-  "brown",
-  "fox",
-  "jumps",
-  "over",
-  "the",
-  "lazy",
-  "dog",
-
-  class Node {
-    constructor(num, word) {
-      this.num = num;
-      this.word = word;
-      this.next = null;
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
-//Creating new nodes
-const node1 = new Node(1);
-const node2 = new Node(2);
-const nodeWord1 = new Node("The");
-const node3 = new Node(3);
-const nodeWord2 = new Node("quick");
-const node4 = new Node(4);
-const nodeWord3 = new Node("brown");
-const node5 = new Node(5);
-const nodeWord4 = new Node("fox");
-const node6 = new Node(6);
-const nodeWord5 = new Node("jumps");
-const node7 = new Node(7);
-const nodeWord6 = new Node("overs");
-const node8 = new Node(8);
-const nodeWord7 = new Node("the");
-const node9 = new Node(9);
-const nodeWord8 = new Node("lazy");
-const node0 = new Node(0);
-const nodeWord9 = new Node("dog");
-
-console.log(node1)
-
-// //Linking the nodes the annoying way
-// node1.next = node2
-// node2.next = node3
-// node3.next = node4
-// node4.next = node5
-// node5.next = node6
-// node6.next = node7
-// node7.next = node8
-// node8.next = node9
-// node9.next = node0
-
-
-// console.log(nums)
-// console.log(words)
+}
 
 class LinkedList {
   constructor(head = null) {
@@ -75,7 +27,7 @@ class LinkedList {
   }
   size(node) {
     let count = 0
-    let node = this.head
+    let currentNode = this.head
     while (node) {
       count++
       currentNode = currentNode.next
@@ -183,6 +135,46 @@ class LinkedList {
     return false
   }
 }
+
+
+//Creating new nodes
+const node1 = new Node(1);
+const node2 = new Node(2);
+const nodeWord1 = new Node("The");
+const node3 = new Node(3);
+const nodeWord2 = new Node("quick");
+const node4 = new Node(4);
+const nodeWord3 = new Node("brown");
+const node5 = new Node(5);
+const nodeWord4 = new Node("fox");
+const node6 = new Node(6);
+const nodeWord5 = new Node("jumps");
+const node7 = new Node(7);
+const nodeWord6 = new Node("overs");
+const node8 = new Node(8);
+const nodeWord7 = new Node("the");
+const node9 = new Node(9);
+const nodeWord8 = new Node("lazy");
+const node0 = new Node(0);
+const nodeWord9 = new Node("dog");
+
+console.log(node1)
+
+// //Linking the nodes the annoying way
+// node1.next = node2
+// node2.next = node3
+// node3.next = node4
+// node4.next = node5
+// node5.next = node6
+// node6.next = node7
+// node7.next = node8
+// node8.next = node9
+// node9.next = node0
+
+
+// console.log(nums)
+// console.log(words)
+
 
 
 
