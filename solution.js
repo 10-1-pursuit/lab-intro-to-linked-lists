2384bad749e392b6898f566327a3fffbae5bb7bc
Kenneth-Parker <kennethkavon@gmail.com>
2024-01-08 10:05:49 -0500

    insert

--- solution.js
+++ solution.js
@@ -1,6 +1,27 @@
 const { nums, words } = require("./data/data.js");
 
+
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
 };
+
