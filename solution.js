2756dbdb721eec388bf6790127a32e15d09efc2a
Tyrell <tyrellkamara-oliver@pursuit.org>
2024-01-09 12:48:36 -0500

    fixed the insert method to get the tests passing

--- solution.js
+++ solution.js
@@ -7,28 +7,28 @@ class Node {
   }
 }
 
-const newNode = new Node(3);
-const newNode2 = new Node(6);
-const newNode3 = new Node(9);
-
-// console.log(firstNode)
+// const newNode = new Node(3);
+// const newNode2 = new Node(6);
+// const newNode3 = new Node(9);
+// console.log(newNode)
 
 class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
 
-  // Takes in a node as a parameter
-  // Adds the node from the param to the end of the list
-  insert(node) {
+  // Takes in data as a parameter (not a node)
+  insert(data) {
     // Initilize a variable to the head of the linkedlist
     let currentNode = this.head;
+    // Create a new instance of the Node class with the data from the param
+    const newNode = new Node(data)
     // Check if there is no head, meaning if the list is empty or head is null,
     if (!currentNode) {
       // If the list is empty the head property is set to the param we are passing in
-      this.head = node;
+      this.head = newNode;
       // Otherwise if there a node after the head, or next is not null create a while loop
-    } else if (currentNode.next) {
+    } else {
       // As long as or while the next node after the head exists or while next is not null keep checking if the next node has a next node after that one.
       while (currentNode.next) {
         // while there is a next set the currentNode which is the head, to the next node
@@ -36,11 +36,8 @@ class LinkedList {
       }
       // The while loop breaks when currentNode.next is false / when next is null, meaning you are at the end of the list.
       // Once at the end just assign the next property to the param you are passing in
-      currentNode.next = node;
-    } else {
-      // Otherwise if there is a head but no next is null, assign the next to the param you are passing
-      currentNode.next = node;
-    }
+      currentNode.next = newNode;
+    } 
   }
 
   size() {
@@ -80,16 +77,21 @@ class LinkedList {
 
 const newList = new LinkedList();
 
-newList.insert(newNode);
-newList.insert(newNode2);
-newList.insert(newNode3);
+// newList.insert(newNode);
+// newList.insert(newNode2);
+// newList.insert(newNode3);
 
-// console.log(newList);
+
+for (let i = 0; i < words.length; i++) {
+  newList.insert(words[i])
+}
+
+console.log(newList);
 // console.log(newList.size());
 // newList.clear();
 
 // console.log(newList.getFirst());
-console.log(newList.getLast());
+// console.log(newList.getLast());
 
 module.exports = {
   Node,
